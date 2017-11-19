1dc42c88b19bc9737f0dda8f832e8cb44945d053
MSypniewski511 <msypniewski511@gmail.com>
2017-11-19 03:42:39 +0000

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,8 +4,14 @@ class VisitorsController < ApplicationController
   def new
     Rails.logger.debug 'DEBUG: '.background(:blue) +  ' entering the new method'
     @owner = Owner.new
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
     Rails.logger.debug 'DEBUG: '.background(:blue) + ' Owner name is ' + Rainbow(@owner.name).background(:red).bright
     #raise 'Deliberate Failure'.color(:red)
+  end
+
+  def colekcja
+    @kols = 1..100
   end
 
 end
